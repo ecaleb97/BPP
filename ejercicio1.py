de225e0c6bfac344c732245cec57df22a2ecb2ff
Caleb Villanueva Zamalloa <calebzamalloa90@gmail.com>
2022-04-18 16:48:25 +0200

    Elementos maximos

--- ejercicio1.py
+++ ejercicio1.py
@@ -3,3 +3,10 @@ cols = 4
 
 m = [[i*cols+j for j in range(cols)] for i in range(row)]
 print(m)
+
+def get_maximo(lista):
+    return max(lista)
+
+elem_maximos = list(map(get_maximo, m))
+
+print('Los elementos mayores son: ', elem_maximos)
