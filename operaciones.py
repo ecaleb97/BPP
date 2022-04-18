dc3caf355e84376d552eb6b847ab87a1bb7d92ea
Caleb Villanueva Zamalloa <calebzamalloa90@gmail.com>
2022-04-18 16:36:20 +0200

    Separacion de letras con compresion de listas

--- operaciones.py
+++ operaciones.py
@@ -1,20 +1,2 @@
-import pdb
-pdb.set_trace()
-
-def suma_cuadrados(n):
-    suma_cuadrados = 0
-    while(n):
-        suma_cuadrados += (n % 10) * (n % 10)
-        n = n // 10
-    return suma_cuadrados
-
-def esNumeroFeliz(n):
-    s = n
-    f = n
-    while(True):
-        s = suma_cuadrados(s)
-        f = suma_cuadrados(suma_cuadrados(f))
-        if s != f:
-            continue
-        else:
-            break
+letras = [i for i in 'Python es el mejor lenguaje de programacion']
+print(letras)
